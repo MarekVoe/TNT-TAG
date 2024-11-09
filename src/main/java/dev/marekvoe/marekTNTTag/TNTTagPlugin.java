8894833275b37d174a376ded47593b0c8a7d2952
MarekVoe <marekhrachovec3@gmail.com>
2024-11-09 16:24:26 +0100

    first commit

---
+++ src/main/java/dev/marekvoe/marekTNTTag/TNTTagPlugin.java
@@ -0,0 +1,17 @@
+package dev.marekvoe.marekTNTTag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TNTTagPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
